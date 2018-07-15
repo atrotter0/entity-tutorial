8a9866c8562011802bcdb33428cdbd7f53e34f0c
Abel Trotter <a_trotter0@hotmail.com>
2018-07-14 18:43:25 -0700

    Create Item.cs and ItemsController

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Linq;
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Index()
+        {
+            List<Item> model = db.Items.ToList();
+            return View(model);
+        }
+    }
+}
